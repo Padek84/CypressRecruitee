f1d2342d27ccbcc3e8abd248965a06b1ad6fb871
DPadek <padewski.dawid@gmail.com>
2021-09-01 00:56:06 +0200

    TASK-2 - the test case created

--- cypress/integration/Tests/Tests.ts
+++ cypress/integration/Tests/Tests.ts
@@ -1,5 +1,7 @@
 /// <reference types="Cypress" />
 
+import CreateFakeDataHelper from '../../helpers/randomTags';
+
 const mainPage = Cypress.env('mainPage');
 const offersSubPage = Cypress.env('offersSubPage');
 
@@ -14,54 +16,54 @@ const password = 'Polska1!';
 
 describe('Recruitment task', () => {
 
-    before(() => {        
+    before(() => {
         cy.visit(mainPage);
+        cy.viewport(1920, 1080)
 
         cy.get(emailFieldId).type(emailAddress);
         cy.get(passwordFieldId).type(password);
         cy.get(buttonClass).click();
-      })
+    })
 
-      beforeEach(() => {
+    beforeEach(() => {
         Cypress.Cookies.preserveOnce(accessTokenCookieName);
-      })
-    
-    it('Create a new job offer', ()=>{
+    })
+
+    it('Create a new job offer', () => {
         cy.visit(offersSubPage);
-        //cy.wait(25000);
+
         cy.contains('New job').click();
-        cy.get('.form-field__input-wrapper').within(()=>{
-            cy.get('[formcontrolname="title"]').within(()=>{
+
+        cy.get('.form-field__input-wrapper').within(() => {
+            cy.get('[formcontrolname="title"]').within(() => {
                 cy.get('[placeholder="Type job title"]').type('Quality Assurance role');
             })
         });
-        
-        cy.get('.underlined.has-clear-button.ng-star-inserted').within(()=>{
+
+        cy.get('.underlined.has-clear-button.ng-star-inserted').within(() => {
             cy.get('[placeholder="Select"]').click();
         })
 
-        cy.get('.container-dropdown.context-alt').within(()=>{
+        cy.get('.container-dropdown.context-alt').within(() => {
             cy.get('.text__slot').contains('Product').click();
         })
 
         cy.get('.redactor-styles.redactor-placeholder.redactor-in.redactor-in-0').type('Job description should be filled in - quality, quality, quality');
 
         cy.get('.redactor-styles.redactor-placeholder.redactor-in.redactor-in-1').type('Job requirements should be also filled in - so here is the text');
-        
-        cy.get('[formcontrolname="countryCode"]').within(()=>{
-            cy.get('[variant="underlined has-dropdown select-input"]').click();            
-        });        
 
-        
+        cy.get('[formcontrolname="countryCode"]').within(() => {
+            cy.get('[variant="underlined has-dropdown select-input"]').click();
+        });
+
         cy.get('[class="ng-star-inserted"]').contains('Poland').click();
-        
 
-        cy.get('[formcontrolname="stateCode"]').within(()=>{
+        cy.get('[formcontrolname="stateCode"]').within(() => {
             cy.get('[variant="underlined has-dropdown select-input"]').click();
         })
-        
+
         cy.get('[class="ng-star-inserted"]').contains('Lubuskie').click();
-        
+
         cy.get('[formcontrolname="city"]').type('Nowe Miasteczko');
 
         cy.get('[formcontrolname="postalCode"]').type('67-124');
@@ -70,45 +72,72 @@ describe('Recruitment task', () => {
 
         cy.get('.theme-default.divider.ng-star-inserted').contains('Senior').click();
 
-        cy.get('[class="form-field__input-wrapper"]').within(()=>{
-            cy.get('[placeholder="Search tags..."]').type('PadekTag');
+        let tagName = CreateFakeDataHelper.getTagRandomName();
+
+        cy.get('[class="form-field__input-wrapper"]').within(() => {
+            cy.get('[placeholder="Search tags..."]').type(tagName);
         })
 
-        cy.get('[class="ng-star-inserted"]').within(()=>{
-            cy.get('[class="text__slot"]').within(()=>{
+        cy.get('[class="ng-star-inserted"]').within(() => {
+            cy.get('[class="text__slot"]').within(() => {
                 cy.get('[class="special-item highlight-value clickable ng-star-inserted"]').contains('Create new tag').click();
             })
         });
 
+        cy.get('.details-wrap').contains('Job details ').scrollIntoView();
 
-        cy.get('[class="form-field__input-wrapper"]').within(()=>{
-            cy.get('[placeholder="Search tags..."]').trigger('keydown', { keyCode: 27});
+        cy.get('[formcontrolname="employmentType"]').within(() => {
+            cy.get('[placeholder="Select"]').click({
+                force: true
+            });
         })
-        
-        cy.get('[formcontrolname="minHours"]').within(()=>{
-            cy.get('[type="text"]').type('25');
-        })
-        
-        cy.get('[formcontrolname="maxHours"]').within(()=>{
-            cy.get('[type="text"]').type('60');
-        })
-        
-        cy.get('[formcontrolname="employmentType"]').click(); 
 
         cy.get('[class="item ng-star-inserted"]').contains('Temporary').click();
 
-        cy.get('[formcontrolname="category"]').click();
+        cy.get('[formcontrolname="category"]').within(() => {
+            cy.get('[placeholder="Select"]').click({
+                force: true
+            });
+        })
 
         cy.get('[class="item ng-star-inserted"]').contains('Accounting').click();
 
-        cy.get('formcontrolname="education"').click();
+        cy.get('[formcontrolname="education"]').within(() => {
+            cy.get('[placeholder="Select"]').click({
+                force: true
+            });
+        })
 
         cy.get('[class="item ng-star-inserted"]').contains('Certification').click();
 
-        cy.get('formcontrolname="experience"').click();
+        cy.get('[formcontrolname="experience"]').within(() => {
+            cy.get('[placeholder="Select"]').click({
+                force: true
+            });
+        })
 
-        cy.get('[class="item ng-star-inserted"]').contains('Entry level').click();
+        cy.get('[class="item ng-star-inserted"]').contains('Entry level').click({
+            force: true
+        });
 
-        cy.get('[class="text-content"]').contains('Application form').click();
+        cy.get('[formcontrolname="minHours"]').within(() => {
+            cy.get('[type="text"]').click({
+                force: true
+            });
+        })
+
+        cy.get('[formcontrolname="minHours"]').within(() => {
+            cy.get('[type="text"]').type('25');
+        })
+
+        cy.get('[formcontrolname="maxHours"]').within(() => {
+            cy.get('[type="text"]').click({
+                force: true
+            }).type('60');
+        })
+
+        cy.get('[class="text-content"]').contains('Application form').click({
+            force: true
+        });
     })
 })
